8c0fa514f4312c4d6641ed341d4210773349324a
Bs1987 <boysax@gmail.com>
2021-05-21 15:55:54 +0200

    Made buttons

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  );
+    return (
+        <div>
+            <h1>Handbags & Purses</h1>
+            <nav>
+                <button>To the collection</button>
+                <button>Shop all bags</button>
+                <button>Pre-order</button>
+            </nav>
+        </div>
+    );
 }
 
 export default App;
