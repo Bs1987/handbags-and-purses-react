e4ff613ce3240cba327ebfb7a7b9e24b5222774a
Bs1987 <boysax@gmail.com>
2021-05-21 16:17:05 +0200

    put hardcoded mockup in product.js

---
+++ src/Product.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Product() {
+    return (
+        <article>
+                <span>
+                    Red label
+                </span>
+            <img src="assets/bag_1.png" alt=""/>
+            <p>
+                bag name
+            </p>
+            <h4>
+                price
+            </h4>
+        </article>
+    );
+}
