321a9368a0e5e6140c20965aa166dd0c4dd6d05c
Bs1987 <boysax@gmail.com>
2021-05-21 15:58:37 +0200

    made onclick function for buttons

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ function App() {
         <div>
             <h1>Handbags & Purses</h1>
             <nav>
-                <button>To the collection</button>
-                <button>Shop all bags</button>
-                <button>Pre-order</button>
+                <button type="button"
+                        onClick={() => console.log("To the collection")}>
+                    To the collection
+                </button>
+                <button type="button"
+                        onClick={() => console.log("Shop all bags")}>
+                    Shop all bags
+                </button>
+                <button type="button"
+                        onClick={() => console.log("Pre-order")}>
+                    Pre-order
+                </button>
+
             </nav>
         </div>
     );
